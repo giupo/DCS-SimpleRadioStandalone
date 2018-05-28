333f22b3babcc813e451de1a0e3c67c5f78bd029
Ciaran Fisher <cfisher88 [at] gmail [dot] com>
2018-05-28 10:34:13 +0100

    Stub
    Stub for Radio Broadcaster

---
+++ DCS-Radio-Broadcast/Bootstrapper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Caliburn.Micro;
+using Ciribob.DCS.SimpleRadio.Standalone.Broadcaster.UI.MainWindow;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using LogManager = NLog.LogManager;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.Broadcaster
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private readonly SimpleContainer _simpleContainer = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+            SetupLogging();
+
+            GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
+
+          //  Analytics.Log("Server", "Startup", Guid.NewGuid().ToString());
+        }
+
+        private void SetupLogging()
+        {  
+            var config = new LoggingConfiguration();
+
+            var fileTarget = new FileTarget();
+            config.AddTarget("file", fileTarget);
+
+            fileTarget.FileName = "${basedir}/serverlog.txt";
+            fileTarget.Layout =
+                @"${longdate} | ${logger} | ${message} ${exception:format=toString,Data:maxInnerExceptionLevel=1}";
+
+#if DEBUG
+            config.LoggingRules.Add( new LoggingRule("*", LogLevel.Debug, fileTarget));
+#else
+            config.LoggingRules.Add( new LoggingRule("*", LogLevel.Info, fileTarget));
+#endif
+           
+            LogManager.Configuration = config;
+        }
+
+
+        protected override void Configure()
+        {
+            _simpleContainer.Singleton<IWindowManager, WindowManager>();
+            _simpleContainer.Singleton<IEventAggregator, EventAggregator>();
+         
+
+            _simpleContainer.Singleton<MainViewModel>();
+
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            var instance = _simpleContainer.GetInstance(service, key);
+            if (instance != null)
+                return instance;
+
+            throw new InvalidOperationException("Could not locate any instances.");
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _simpleContainer.GetAllInstances(service);
+        }
+
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            IDictionary<string, object> settings = new Dictionary<string, object>
+            {
+              //  {"Icon", new BitmapImage(new Uri("pack://application:,,,/SR-Server;component/server-10.ico"))},
+                {"ResizeMode", ResizeMode.CanMinimize}
+            };
+            //create an instance of serverState to actually start the server
+         //   _simpleContainer.GetInstance(typeof(ServerState), null);
+
+            DisplayRootViewFor<MainViewModel>(settings);
+
+           // UpdaterChecker.CheckForUpdate();
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _simpleContainer.BuildUp(instance);
+        }
+
+
+        protected override void OnExit(object sender, EventArgs e)
+        {
+//            var serverState = (ServerState) _simpleContainer.GetInstance(typeof(ServerState), null);
+           
+        }
+    }
+}
---
+++ DCS-Radio-Broadcast/UI/MainWindow/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿
+using Caliburn.Micro;
+using NLog;
+using LogManager = NLog.LogManager;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.Broadcaster.UI.MainWindow
+{
+    public sealed class MainViewModel : Screen
+    {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IWindowManager _windowManager;
+        private readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public MainViewModel(IWindowManager windowManager, IEventAggregator eventAggregator)
+        {
+            _windowManager = windowManager;
+           _eventAggregator = eventAggregator;
+        
+            _eventAggregator.Subscribe(this);
+
+            DisplayName = "DCS-SRS Broadcaster - ";// + UpdaterChecker.VERSION;
+
+       //     Logger.Info("DCS-SRS Broadcaster Running - " + UpdaterChecker.VERSION);
+        }
+
+    
+  
+    }
+}
